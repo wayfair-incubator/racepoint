27145ab103d18c5fbc79d39d9465095cfe7e49f7
Steve Pember <spember@wayfair.com>
2022-02-04 15:19:14 -0500

    -created a basic 'endpoint' object and approach to handling urls within the racer -shifted the fetch and fingerprint endpoints into a dedicated directory
    -created a basic 'endpoint' object and approach to handling urls within the racer
    -shifted the fetch and fingerprint endpoints into a dedicated directory

---
+++ packages/racer/src/controllers/fetch.ts
@@ -0,0 +1,37 @@
+/**
+ * Do a straight pass-through fetch of a url through the proxy.
+ * This should be used for testing purposes. Has an optional parameter
+ * to control just how much text comes back
+ */
+import fetch from 'node-fetch';
+import {RegisteredEndpoint, RaceProxyHttpsAgent} from './common';
+
+const fetchUrlFromProxy = async (
+  url: string,
+  requestedLength: number = 100
+) => {
+  const response = await fetch(url, {
+    agent: RaceProxyHttpsAgent,
+  });
+  const body = await response.text();
+  console.log('Here is the response:', body.slice(0, requestedLength));
+  return body.slice(0, requestedLength);
+};
+
+export const FetchEndpoint: RegisteredEndpoint = {
+  path: '/fetch',
+  handler: async (req, res, parsedUrl) => {
+    const target = parsedUrl.query['url'];
+    if (target === undefined) {
+      throw new Error('This endpoint requires a url');
+    }
+
+    const html = await fetchUrlFromProxy(
+      target.toString(),
+      parsedUrl.query['size']
+        ? parseInt(parsedUrl.query['size'].toString())
+        : undefined
+    );
+    return {html};
+  },
+};
--- packages/racer/src/index.ts
+++ packages/racer/src/index.ts
@@ -1,34 +1,10 @@
-import url from 'url';
-import http from 'http';
-import {IncomingMessage, ServerResponse} from 'http';
-import {fetchUrlFromProxy} from './scenarios/fetch';
-import {fetchFingerprint} from './fingerprint';
+import {initialize} from './server';
 
-// two modes: one, starts the racer server. This is the chief mode of
-// operation and is what will be hit by external services
-// second is a test 'fetch' mode which will grab the SPIFKey and execute a fetch through the
-// proxy
+//start server, parse url, match the first segment against one of our controllers
 
-const runProxy = async () => {
-  const server = await http.createServer(
-    async (req: IncomingMessage, res: ServerResponse) => {
-      console.log('received a hit to ', req.url);
-
-      if (req.url && req.url.startsWith('/fetch')) {
-        console.log('fetching! ', req.headers['host']);
-        const queryObject = url.parse(req.url, true).query;
-        fetchUrlFromProxy(queryObject['url']!!.toString());
-        res.statusCode = 200;
-      } else if (req.url && req.url.startsWith('/fingerprint')) {
-        console.log(fetchFingerprint());
-      } else {
-        res.statusCode = 404;
-      }
-
-      res.end();
-    }
-  );
+const goSpeedRacer = async () => {
+  const server = initialize();
   console.log('Go Speed Racer Go');
   server.listen(3000);
 };
-runProxy();
+goSpeedRacer();
