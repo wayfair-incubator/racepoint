058f2e29518dd8ef96b1191d233403008e051211
Steve Pember <spember@wayfair.com>
2022-02-07 16:07:04 -0500

    -updates per jason -added an http Agent, and select usage based on protocol

--- packages/racer/src/controllers/common.ts
+++ packages/racer/src/controllers/common.ts
@@ -1,5 +1,6 @@
 import url from 'url';
 import https from 'https';
+import http, {Agent} from 'http';
 import {IncomingMessage, ServerResponse} from 'http';
 
 export interface RegisteredEndpoint {
@@ -16,3 +17,18 @@ export const RaceProxyHttpsAgent = new https.Agent({
   host: 'raceproxy',
   port: 443,
 });
+
+export const RaceProxyHttpAgent = new http.Agent({
+  host: 'raceproxy',
+  port: 80,
+});
+
+export const selectAgentForProtocol = (
+  targetUrl: string
+): http.Agent | https.Agent => {
+  if (targetUrl.startsWith('https')) {
+    return RaceProxyHttpsAgent;
+  } else {
+    return RaceProxyHttpAgent;
+  }
+};
