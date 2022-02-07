43502dc69f473289e066b4ee6da60a0147506c43
Jason Papanicholas <jpapanicholas@wayfair.com>
2022-02-07 12:30:40 -0500

    Porting over tests for proxy

---
+++ packages/proxy/test/proxy.spec.ts
@@ -0,0 +1,118 @@
+/*
+  Proxy Mocha Test
+*/
+import {
+  buildHttpReverseProxy,
+  buildHttpsReverseProxy,
+} from '../src/reverse-proxy';
+import {ProxyCache} from '../src/proxy-cache';
+import {Server} from 'http';
+import chai, {expect} from 'chai';
+import chaiHttp from 'chai-http';
+import rp from 'request-promise';
+import {CACHE_KEY_HEADER} from '../src/cache-helpers';
+
+chai.use(chaiHttp);
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+// @TODO instead of going into the wild internet for a URL,
+// we should be stubbing out a response using a mock API like wiremock
+// Follow-up ticket: SFPERF-2250
+const testOptions = {
+  url: 'http://example.com/',
+  method: 'GET',
+  proxy: 'http://localhost:80',
+  resolveWithFullResponse: true,
+};
+
+describe('HTTP Server', () => {
+  let httpProxy: Server;
+  let testCache: ProxyCache;
+
+  before(async () => {
+    testCache = new ProxyCache();
+    httpProxy = await buildHttpReverseProxy(testCache);
+    httpProxy.listen(80);
+  });
+
+  after(() => {
+    httpProxy.close();
+  });
+
+  it('should start an HTTP proxy server on a given port', async () => {
+    expect(httpProxy.listening).to.be.true;
+  });
+
+  describe('Caching works', async () => {
+    it('should receive response from server', async () => {
+      await rp(testOptions)
+        .then((response) => {
+          expect(response.statusCode).to.equal(200);
+          // Expect some blob of HTML
+          expect(response.body.length > 10);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    });
+
+    it('should receive cached response from server upon retry', async () => {
+      await rp(testOptions)
+        .then((response) => {
+          expect(response.statusCode).to.equal(200);
+          expect(response.body.length > 0);
+          expect(response.headers).to.have.property(CACHE_KEY_HEADER);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    });
+  });
+});
+
+describe('HTTPS Server', () => {
+  let httpsProxy: Server;
+  let testCache: ProxyCache;
+
+  beforeEach(async () => {
+    testCache = new ProxyCache();
+    httpsProxy = await buildHttpsReverseProxy(testCache);
+    httpsProxy.listen(443);
+  });
+
+  afterEach(() => {
+    if (httpsProxy) {
+      httpsProxy.close();
+    }
+  });
+
+  it('should start an HTTPS proxy server on a given port', async () => {
+    expect(httpsProxy.listening).to.be.true;
+  });
+
+  it('should receive a fingerprint when requesting HTTPS proxy', async () => {
+    let fingerprint;
+
+    await rp({
+      uri: 'https://localhost:443/fingerprint',
+      headers: {
+        'User-Agent': 'Request-Promise',
+        host: 'raceproxy',
+      },
+      resolveWithFullResponse: true,
+    })
+      .then((response) => {
+        console.log('DID WE GET HERE');
+        const parsedBody = JSON.parse(response.body);
+        fingerprint = parsedBody.spkiFingerprint;
+        expect(response.statusCode).to.equal(200);
+        expect(fingerprint).to.have.lengthOf.at.least(1);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+
+  // @TODO caching tests similar to HTTP when we figure out insecure requests
+});
