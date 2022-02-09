13ecb87a356df9587542012f269f6143635e5f8a
Steve Pember <spember@wayfair.com>
2022-02-09 13:53:44 -0500

    -updated comments

--- packages/racer/src/controllers/profile.ts
+++ packages/racer/src/controllers/profile.ts
@@ -26,31 +26,20 @@ const isValid = (context: RaceContext): boolean => {
 const maybeRunLighthouse = async (
   context: RaceContext
 ): Promise<EndpointResponse<object>> => {
-  // check for lock. try and acquire, if it doesn't lock, reject the request
-  //
-  // const locked = await UsageLock.getInstance().tryAcquire()
-  // if (locked) {
-
-  // }
+  // try to get a lock; if LH is currently in use, send back a 503!
   return UsageLock.getInstance()
     .tryAcquire()
     .then((obtained) => {
-      //   const response = new EndpointResponse({});
       // return a promise, either resolving the locked response or the initating of lighthouse
       if (obtained === false) {
         return new EndpointResponse({
           error: 'Racer is currently in use',
         }).withStatusCode(503);
       } else {
-        // kick start Lighthouse
-        // const jobId = submitLighthouseRun(context.targetUrl);
-        // console.log('Job Id ', jobId);
-        // response.withBody({jobId});
         return submitLighthouseRun(context.targetUrl).then(
           (jobId) => new EndpointResponse({jobId})
         );
       }
-      //   return Promise.resolve(response);
     });
 
   // get lock, then, if locked return failure, otherwise execute lighthouse
