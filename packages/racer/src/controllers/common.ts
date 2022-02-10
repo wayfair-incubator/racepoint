e346cca89cfcacc8ad7471f355ec0e452610a4de
Steve Pember <spember@wayfair.com>
2022-02-10 10:14:03 -0500

    -added headers support to the http response object

--- packages/racer/src/controllers/common.ts
+++ packages/racer/src/controllers/common.ts
@@ -3,6 +3,9 @@ import https from 'https';
 import http from 'http';
 import {IncomingMessage, ServerResponse} from 'http';
 
+/**
+ * Used to Register a Listener / handler for a given url
+ */
 export interface RegisteredEndpoint<T> {
   path: string;
   method: string;
@@ -13,9 +16,16 @@ export interface RegisteredEndpoint<T> {
   ) => Promise<EndpointResponse<T>>;
 }
 
+/**
+ * A response context object, used to signal to the main server handler function the status, headers, and body back to the
+ * http response
+ */
 export class EndpointResponse<T> {
   private statusCode = 200;
   private body: T;
+  private headers: {[name: string]: string} = {
+    'Content-Type': 'application/json',
+  };
 
   constructor(body: T) {
     this.body = body;
@@ -31,6 +41,11 @@ export class EndpointResponse<T> {
     return this;
   }
 
+  public withHeader(name: string, value: string): EndpointResponse<T> {
+    this.headers[name] = value;
+    return this;
+  }
+
   public getStatusCode() {
     return this.statusCode;
   }
@@ -38,31 +53,56 @@ export class EndpointResponse<T> {
   public getBody() {
     return this.body;
   }
+
+  public getHeaders() {
+    return this.headers;
+  }
 }
 
+/**
+ * An Https agent specific for talking to the Race Proxy
+ */
 export const RaceProxyHttpsAgent = new https.Agent({
   rejectUnauthorized: false,
   host: 'raceproxy',
   port: 443,
 });
 
+/**
+ * An Http agent specific for talking to the Race Proxy
+ */
 export const RaceProxyHttpAgent = new http.Agent({
   host: 'raceproxy',
   port: 80,
 });
 
+/**
+ * Chooses the Agent to use based on the url protocol
+ *
+ * @param targetUrl A url string we're attempting to reach out to
+ * @returns the appropriate Agent for the protocol
+ */
 export const selectAgentForProtocol = (
   targetUrl: string
 ): http.Agent | https.Agent => {
   if (targetUrl.startsWith('https')) {
     return RaceProxyHttpsAgent;
-  } else {
+  } else if (targetUrl.startsWith('http')) {
     return RaceProxyHttpAgent;
+  } else {
+    throw new Error(`Unknown protocol for url ${targetUrl}`);
   }
 };
 
+/**
+ *
+ * @param request the IncomingMessage we want to extract a payload from
+ * @param parser The parser to use to process the payload. Defaults to JSON.parse
+ * @returns any
+ */
 export const extractBodyFromRequest = (
-  request: IncomingMessage
+  request: IncomingMessage,
+  parser: (data: string) => any = JSON.parse
 ): Promise<object> =>
   new Promise((resolve, reject) => {
     let payload = '';
@@ -70,6 +110,6 @@ export const extractBodyFromRequest = (
       payload += chunk;
     });
     request.on('end', () => {
-      resolve(JSON.parse(payload));
+      resolve(parser(payload));
     });
   });
