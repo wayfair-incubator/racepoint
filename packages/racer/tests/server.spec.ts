10364e73024ff002081da9a9dd3cd24bcd74cf25
Steve Pember <spember@wayfair.com>
2022-02-10 19:11:31 -0500

    -added a route handler to extract ids from matching routes
    -wired up the ability to fetch both the json and html report for a result

---
+++ packages/racer/tests/server.spec.ts
@@ -0,0 +1,61 @@
+import chai, {expect} from 'chai';
+import {RouteMatcher} from '../src/server/routeMatcher';
+import {RegisteredEndpoint, EndpointResponse} from '../src/server/utils';
+
+describe('Route Parsing', () => {
+  const router = new RouteMatcher([
+    {
+      path: '/foo',
+      method: 'GET',
+      handler: () => {
+        return Promise.resolve(new EndpointResponse({}));
+      },
+    },
+    {
+      path: '/foo/{fooId}',
+      method: 'GET',
+      handler: () => {
+        return Promise.resolve(new EndpointResponse({}));
+      },
+    },
+    {
+      path: '/bar/{id}',
+      method: 'POST',
+      handler: () => {
+        return Promise.resolve(new EndpointResponse({}));
+      },
+    },
+    {
+      path: '/bar/{id}/testing/{tId}/baz/{bId}',
+      method: 'POST',
+      handler: () => {
+        return Promise.resolve(new EndpointResponse({}));
+      },
+    },
+  ]);
+
+  it('should match urls with no vars', () => {
+    const result = router.match('/foo', 'GET');
+    expect(result).to.not.be.undefined;
+    expect(result!!.endpoint.path).to.be.equal('/foo');
+  });
+
+  it('should match urls with a single var', () => {
+    const result = router.match('/foo/123', 'GET');
+    expect(result).to.not.be.undefined;
+    expect(result!!.args['fooId']).to.be.equal('123');
+  });
+
+  it('should not urls with the wrong method', () => {
+    expect(router.match('/foo', 'POST')).to.be.undefined;
+    expect(router.match('/bar/123', 'GET')).to.be.undefined;
+  });
+
+  it('should match urls with multiple vars', () => {
+    const result = router.match('/bar/b25/testing/200/baz/1', 'POST');
+    expect(result).to.not.be.undefined;
+    expect(result!!.args['id']).to.be.equal('b25');
+    expect(result!!.args['tId']).to.be.equal('200');
+    expect(result!!.args['bId']).to.be.equal('1');
+  });
+});
