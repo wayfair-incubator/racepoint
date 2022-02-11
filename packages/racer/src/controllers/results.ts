2c77d651bc81cee208b3742fd31379827f29c9ae
Steve Pember <spember@wayfair.com>
2022-02-10 19:11:31 -0500

    -added a route handler to extract ids from matching routes
    -wired up the ability to fetch both the json and html report for a result

--- packages/racer/src/controllers/results.ts
+++ packages/racer/src/controllers/results.ts
@@ -3,15 +3,50 @@
  * Supports fetching html and json results
  *
  */
-import {RegisteredEndpoint, EndpointResponse} from './common';
-import {LighthouseResultsRepository} from '../profiling/repository';
+import {RegisteredEndpoint, EndpointResponse} from '../server/utils';
+import {
+  LighthouseResultsRepository,
+  RepositoryRecord,
+} from '../profiling/repository';
+import {IncomingMessage} from 'http';
 
 export const ResultsEndpoint: RegisteredEndpoint<object> = {
-  path: '/results',
+  path: '/results/{resultId}',
   method: 'GET',
-  handler: async (req, res, parsedUrl) => {
-    console.log(req.headers);
-    console.log(parsedUrl);
-    return new EndpointResponse({});
+  handler: async (req, res, parsedUrl, args) => {
+    return LighthouseResultsRepository.read(
+      parseInt(args!!['resultId']!!)
+    ).then((record) => {
+      let response = new EndpointResponse({});
+      if (record) {
+        response = selectResponseTypeByHeader(req, record);
+      } else {
+        response.withStatusCode(404);
+      }
+      return Promise.resolve(response);
+    });
   },
+};
+
+const CONTENT_TYPE = 'content-type';
+const MIME_HTML = 'text/html';
+
+const selectResponseTypeByHeader = (
+  req: IncomingMessage,
+  record: RepositoryRecord
+): EndpointResponse<any> => {
+  if (
+    req.headers[CONTENT_TYPE] &&
+    req.headers[CONTENT_TYPE]!!.startsWith(MIME_HTML)
+  ) {
+    return new EndpointResponse(record.results.report).withHeader(
+      CONTENT_TYPE,
+      MIME_HTML
+    );
+  } else {
+    return new EndpointResponse(record.results.lhr).withHeader(
+      CONTENT_TYPE,
+      'application/json'
+    );
+  }
 };
