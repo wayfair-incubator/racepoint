6cc133b8d14610268e1974ed6941e5d6428f82ca
Jason Papanicholas <jpapanicholas@wayfair.com>
2022-03-04 15:56:49 -0500

    Fixing fake test

--- packages/racer/tests/racer.spec.ts
+++ packages/racer/tests/racer.spec.ts
@@ -1,5 +1,7 @@
-describe('', () => {
-  it('', () => {
-    expect(true).toBe(true);
+import {expect} from 'chai';
+
+describe('Fake test', () => {
+  it('holds the place of a real test', () => {
+    expect(2 + 2 === 4).to.be.true;
   });
 });
