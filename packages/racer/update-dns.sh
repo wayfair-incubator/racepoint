f4d02abbd033c133940118ecdf652b3d4297e415
Steve Pember <spember@wayfair.com>
2022-03-18 11:57:10 -0400

    -fingers-crossed fix which replaces the docker dns resolution lookup of raceproxy from within racer to use the dig command instead of the ancient nslookup

--- packages/racer/update-dns.sh
+++ packages/racer/update-dns.sh
@@ -5,7 +5,8 @@
 # * updates the dnsmasq config with that information
 # * updates the /etc/resolv.conf with localhost, thus making it such that this system is its own dns server
 echo "Updating ip address in order to treat local dnsmasq as our dns resolver and route all calls to raceproxy"
-ip=$(nslookup raceproxy | awk '/Address:/ {split($2,d,":");print d[1]}' | tail -n 1)
+# the next line after the 'Answer Section' contains the first match of the domain and its ip
+ip=$(dig raceproxy | awk '/ANSWER SECTION/ {getline;print $5;}')
 
 echo "The ip in question is $ip"
 
@@ -16,9 +17,7 @@ listen-address=::1,127.0.0.1
 address=/#/$ip" >> /etc/dnsmasq.conf
 
 echo "dnsmasq config updated. Replacing /etc/resolv.conf"
-
-# rm /etc/resolv.conf
-sed '1,2d' /etc/resolv.conf
-
+# resolv.conf is actively used by a special mount created by the Docker environment. We cannot cp or rm the file, but we can
+# replace all the text inside
 echo "nameserver 127.0.0.1
-options ndots:0" >> /etc/resolv.conf 
+options ndots:0" > /etc/resolv.conf 
