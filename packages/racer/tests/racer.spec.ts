0192cfd63161134b0cc1cf0eadde879b28a0b702
Jason Papanicholas <jpapanicholas@wayfair.com>
2022-04-18 12:34:48 -0400

    Fix test

--- packages/racer/tests/racer.spec.ts
+++ packages/racer/tests/racer.spec.ts
@@ -2,6 +2,6 @@ import {expect} from 'chai';
 
 describe('Fake test', () => {
   it('holds the place of a real test', () => {
-    expect(2 + 3 === 4).to.be.true;
+    expect(2 + 2 === 4).to.be.true;
   });
 });
