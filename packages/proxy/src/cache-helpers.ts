f68469e4d90e393d2d4f23f1f2e854074f0792b1
Jason Papanicholas <jpapanicholas@wayfair.com>
2022-05-18 16:07:05 -0400

    Working fetching

--- packages/proxy/src/cache-helpers.ts
+++ packages/proxy/src/cache-helpers.ts
@@ -1,10 +1,23 @@
-import {IncomingMessage} from 'http';
-import {Http2ServerRequest} from 'http2';
+import http2, {Http2ServerRequest, Http2ServerResponse} from 'http2';
+import http, {IncomingMessage, ServerResponse} from 'http';
 import {ProxyCache} from './proxy-cache';
 import {StatusCodes} from 'http-status-codes';
 import hash from 'object-hash';
 
 export const CACHE_KEY_HEADER = 'll-cache-key';
+
+const {HTTP2_HEADER_PATH, HTTP2_HEADER_STATUS} = http2.constants;
+
+type RequestType = IncomingMessage | Http2ServerRequest;
+type ResponseType = ServerResponse | Http2ServerResponse;
+
+const isHttpRequest = (obj: RequestType): obj is IncomingMessage => {
+  return (obj as IncomingMessage).httpVersion.startsWith('1');
+};
+
+const isHttpResponse = (obj: ResponseType): obj is ServerResponse => {
+  return (obj as ServerResponse).hasOwnProperty('shouldKeepAlive');
+};
 
 /**
  * Extract the body from a request or response
@@ -32,21 +45,22 @@ export const extractBody = async (
  * @returns
  */
 export const calculateCacheKey = (
-  request: IncomingMessage,
-  requestBody: Buffer
+  request: IncomingMessage | Http2ServerRequest,
+  requestData: Buffer
 ): string => {
   const payload = {
     headers: request.headers,
     url: request.url,
   };
+  const baseUrl = isHttpRequest(request)
+    ? `${request.headers.host}${request.url}`
+    : `${request.authority}${request.url}`;
 
   // If there's any request body from POST/GET/etc. include it in the key
-  if (requestBody.toString().length > 0) {
-    return `${request?.headers['host']}${request?.url}_${hash(
-      requestBody.toJSON()
-    )}`;
+  if (requestData.toString().length > 0) {
+    return `${baseUrl}_${hash(requestData.toJSON())}`;
   } else {
-    return `${request?.headers['host']}${request?.url}_${hash(payload)}`;
+    return `${baseUrl}_${hash(payload)}`;
   }
 };
 
@@ -79,24 +93,20 @@ export const cacheExtractedProxyResponse = async (
   new Promise((resolve) => {
     const key = originalRequest.headers[CACHE_KEY_HEADER] as string | undefined;
 
-    // if (key && !cacheInstance.contains(key)) {
-    //   console.log(`💾 Writing data to cache - ${trimKey(key)}`);
-    //   cacheInstance.write(key, {
-    //     headers: {
-    //       ...proxyResponse.headers,
-    //       [CACHE_KEY_HEADER]: key,
-    //     },
-    //     status: proxyResponse.statusCode
-    //       ? proxyResponse.statusCode
-    //       : StatusCodes.OK,
-    //     data: proxyBodyData,
-    //   });
-    // }
-    console.log(
-      '💽 Data to cache is:',
-      proxyBodyData.slice(0, 50),
-      proxyBodyData.slice(proxyBodyData.length - 50, proxyBodyData.length)
-    );
+    if (key && !cacheInstance.contains(key)) {
+      console.log(`💾 Writing data to cache - ${trimKey(key)}`);
+      cacheInstance.write(key, {
+        headers: {
+          ...proxyResponse.headers,
+          [CACHE_KEY_HEADER]: key,
+        },
+        status: proxyResponse.statusCode
+          ? proxyResponse.statusCode
+          : StatusCodes.OK,
+        data: proxyBodyData,
+      });
+    }
+
     resolve(proxyBodyData);
   });
 
--- packages/proxy/src/reverse-proxy.ts
+++ packages/proxy/src/reverse-proxy.ts
@@ -1,10 +1,10 @@
 /*
   Functions for initializing servers
 */
-// @ts-nocheck
+
 import https from 'https';
 import http2, {Http2ServerRequest, Http2ServerResponse} from 'http2';
-import http, {IncomingMessage, ServerResponse, ClientRequest} from 'http';
+import http, {IncomingMessage, ServerResponse} from 'http';
 import Server from 'http-proxy';
 import {ProxyCache} from './proxy-cache';
 import {
@@ -13,30 +13,34 @@ import {
   calculateCacheKey,
   trimKey,
 } from './cache-helpers';
-import {buildProxyWorker, handleProxyResponse} from './proxy-worker';
+import {buildProxyWorker} from './proxy-worker';
 import {generateCACertificate} from './tls';
 import {http as redirectHttps, RedirectableRequest} from 'follow-redirects';
-import {keys} from 'object-hash';
-import tls from 'tls';
 
 const {HTTP2_HEADER_PATH, HTTP2_HEADER_STATUS} = http2.constants;
+
+type RequestType = IncomingMessage | Http2ServerRequest;
+type ResponseType = ServerResponse | Http2ServerResponse;
+
+const isHttpRequest = (obj: RequestType): obj is IncomingMessage => {
+  return (obj as IncomingMessage).httpVersion.startsWith('1');
+};
+
+const isHttpResponse = (obj: ResponseType): obj is ServerResponse => {
+  return (obj as ServerResponse).hasOwnProperty('shouldKeepAlive');
+};
 
 export const handleIncomingRequest = async ({
   request,
   response,
   cache,
-  proxy,
   handleUncached,
-  isHttp2,
 }: {
-  request: IncomingMessage | Http2ServerRequest;
-  response: ServerResponse | Http2ServerResponse;
+  request: RequestType;
+  response: ResponseType;
   cache: ProxyCache;
-  proxy: Server;
   handleUncached: Function;
-  isHttp2: boolean;
 }) => {
-  console.log('Request', request.scheme);
   const requestData = await extractBody(request);
   const cacheKey = calculateCacheKey(request, requestData);
 
@@ -46,95 +50,80 @@ export const handleIncomingRequest = async ({
     const cachedResponse = cache.read(cacheKey)!!;
 
     response.writeHead(cachedResponse.status, cachedResponse.headers);
+    // @ts-ignore
     response.write(cachedResponse.data);
     response.end();
   } else {
-    if (isHttp2) {
-      console.log(
-        `📡 New request for https://${request.authority}${request.url}`
-      );
+    console.log(
+      `✅ Key created - ${trimKey(cacheKey)}`,
+      requestData.toString().length > 0
+        ? `\nWith data - ${requestData.toString()}`
+        : ''
+    );
+    // Add this key for correlation
+    request.headers[CACHE_KEY_HEADER] = cacheKey;
 
-      const client = http2.connect(`${request.scheme}://${request.authority}`);
-
-      client.on('error', (err) => console.error(err));
-
-      // From here, use 'client' to do HTTP/2 as normal through the tunnel
-      const req = client.request({
-        [HTTP2_HEADER_PATH]: request?.url,
-        ':method': request.headers[':method'],
-        ':authority': request.headers[':authority'],
-        ':scheme': 'https',
-        ':path': request.url,
-        ...request.headers,
-      });
-
-      // To fetch the response body, we set the encoding
-      // we want and initialize an empty data string
-      // req.setEncoding('utf8');
-
-      // since we don't have any more data to send as
-      // part of the request, we can end it
-      req.end();
-
-      const headersToWrite = {};
-      let statusCode = 200;
-
-      // This callback is fired once we receive a response
-      // from the server
-      req.on('response', (headers) => {
-        // we can log each response header here
-        for (const name in headers) {
-          if (!name.startsWith(':')) {
-            headersToWrite[name] = headers[name];
-          }
-          if (name === HTTP2_HEADER_STATUS) {
-            statusCode = headers[name];
-          }
-        }
-
-        const bodyData: Buffer[] = [];
-
-        req.on('data', (chunk: Buffer) => {
-          bodyData.push(chunk);
-        });
-
-        // Once the response is finished, log the entire data
-        // that we received
-        req.on('end', () => {
-          // In this case, we don't want to make any more
-          // requests, so we can close the session
-          client.close();
-          // console.log('📀 Data is ', bodyData);
-
-          response.writeHead(statusCode, headersToWrite);
-          response.write(Buffer.concat(bodyData));
-          response.end();
-        });
-      });
-
-      req.on('error', (err) => console.error('Request error', err));
-    } else {
-      console.log(
-        `✅ Key created - ${trimKey(cacheKey)}`,
-        requestData.toString().length > 0
-          ? `\nWith data - ${requestData.toString()}`
-          : ''
-      );
-      // If we don't have it, we need to get it and cache it
-      const url = request.url || '';
-
-      // @TODO why do some request URLs include the hostname, but not others?
-      const destinationUrl = url.startsWith('http')
-        ? url
-        : `https://${request.headers.host}${url}`;
-
-      // Add this key for correlation
-      request.headers[CACHE_KEY_HEADER] = cacheKey;
-
-      // Handle the response if it's not cached
-      await handleUncached({request, response, proxy, target: destinationUrl});
-    }
+    // Handle the response if it's not cached
+    await handleUncached({request, response});
   }
+};
+
+const http2Proxy = ({
+  request,
+  response,
+}: {
+  request: Http2ServerRequest;
+  response: Http2ServerResponse;
+}) => {
+  const client = http2.connect(`${request.scheme}://${request.authority}`);
+
+  client.on('error', (err) => console.error('Client error', err));
+
+  const req = client.request({
+    [HTTP2_HEADER_PATH]: request?.url || '',
+    ':method': request.headers[':method'],
+    ':authority': request.headers[':authority'],
+    ':scheme': 'https',
+    ':path': request.url,
+    ...request.headers,
+  });
+  // since we don't have any more data to send as
+  // part of the request, we can end it
+  req.end();
+
+  req.on('response', (headers) => {
+    console.log(`📥 Response received for ${trimKey(request.url)}`);
+
+    const headersToWrite: any = {};
+
+    for (const name in headers) {
+      // Remove psuedo-headers
+      if (!name.startsWith(':')) {
+        headersToWrite[name] = headers[name];
+      }
+    }
+
+    const bodyData: Buffer[] = [];
+
+    req.on('data', (chunk: Buffer) => {
+      bodyData.push(chunk);
+    });
+
+    req.on('end', () => {
+      // @ts-ignore Typescript doesn't think statusCode is always number
+      const statusCode: number = headers[HTTP2_HEADER_STATUS]
+        ? headers[HTTP2_HEADER_STATUS]
+        : 0;
+      // In this case, we don't want to make any more
+      // requests, so we can close the session
+      client.close();
+      response.writeHead(statusCode, headersToWrite);
+      response.write(Buffer.concat(bodyData));
+      response.end();
+    });
+  });
+
+  req.on('error', (err) => console.error('Request error', err));
 };
 
 /*
@@ -146,14 +135,15 @@ const proxyTrueDestination = ({
   request,
   response,
   proxy,
-  target,
 }: {
   request: IncomingMessage;
   response: ServerResponse;
   proxy: Server;
-  target: string;
 }) => {
-  console.log(`🌐 Proxying for URL - ${trimKey(request?.url || '')}`);
+  // Should this ever be http?
+  const target = `https://${request.headers.host}${request.url}`;
+
+  console.log(`🌐 Proxying for URL - ${trimKey(target)}`);
   // Have the proxy get that URL
   proxy.web(request, response, {
     target,
@@ -169,11 +159,15 @@ export const buildHttpReverseProxy = async (cache: ProxyCache) => {
   const requestListener = async function (request: any, response: any) {
     handleIncomingRequest({
       cache,
-      proxy,
       request,
       response,
-      handleUncached: proxyTrueDestination,
-      isHttp2: false,
+      handleUncached: ({
+        request,
+        response,
+      }: {
+        request: IncomingMessage;
+        response: ServerResponse;
+      }) => proxyTrueDestination({request, response, proxy}),
     });
   };
 
@@ -196,13 +190,17 @@ export const buildHttpsReverseProxy = async (cache: ProxyCache) => {
   });
 
   server.on('request', async (request, response) => {
-    console.log('Request received 1');
     await handleIncomingRequest({
       cache,
-      proxy,
       request,
       response,
-      handleUncached: proxyTrueDestination,
+      handleUncached: ({
+        request,
+        response,
+      }: {
+        request: IncomingMessage;
+        response: ServerResponse;
+      }) => proxyTrueDestination({request, response, proxy}),
     });
   });
 
@@ -217,7 +215,6 @@ export const buildHttpsReverseProxy = async (cache: ProxyCache) => {
   Build a HTTPS reverse proxy server instance
 */
 export const buildHttp2ReverseProxy = async (cache: ProxyCache) => {
-  // const proxy = buildProxyWorker({ cache });
   const {key, cert} = await generateCACertificate();
 
   const server = http2.createSecureServer({
@@ -231,11 +228,9 @@ export const buildHttp2ReverseProxy = async (cache: ProxyCache) => {
   server.on('request', async (request, response) => {
     await handleIncomingRequest({
       cache,
-      // proxy,
       request,
       response,
-      handleUncached: proxyTrueDestination,
-      isHttp2: true,
+      handleUncached: http2Proxy,
     });
   });
 
@@ -251,7 +246,7 @@ export const buildHttp2ReverseProxy = async (cache: ProxyCache) => {
     console.log('🌵 New session!');
   });
 
-  server.on('timeout', (a) => {
+  server.on('timeout', (a: any) => {
     console.log('🍅 Timeout!', a);
   });
 
