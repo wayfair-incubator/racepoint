deefe502a5a655f3bd9e42837f61565385bb238e
Jason Papanicholas <jpapanicholas@wayfair.com>
2022-06-13 13:19:18 -0400

    Improve test

--- flows/my-test.js
+++ flows/my-test.js
@@ -4,8 +4,24 @@ class whateverYouWant {
   async connect(browser) {
     // return new Promise(async (resolve, reject) => {
     const page = await browser.newPage();
-    const flow = await api.startFlow(page, {name: 'Single Navigation'});
-    await flow.navigate('http://google.com/');
+
+    const flow = await api.startFlow(page, {name: 'Squoosh snapshots'});
+
+    await page.goto('https://squoosh.app/', {waitUntil: 'networkidle0'});
+
+    // Wait for first demo-image button, then open it.
+    const demoImageSelector = 'ul[class*="demos"] button';
+    await page.waitForSelector(demoImageSelector);
+    await flow.snapshot({stepName: 'Page loaded'});
+    await page.click(demoImageSelector);
+
+    // Wait for advanced settings button in UI, then open them.
+    const advancedSettingsSelector = 'form label[class*="option-reveal"]';
+    await page.waitForSelector(advancedSettingsSelector);
+    await flow.snapshot({stepName: 'Demo loaded'});
+    await page.click(advancedSettingsSelector);
+
+    await flow.snapshot({stepName: 'Advanced settings opened'});
 
     await browser.close();
 
