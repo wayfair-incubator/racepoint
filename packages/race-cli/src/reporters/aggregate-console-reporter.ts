f00dd7dcf739fdfdea74dbf1cc7b8a0edd3df80a
Jason Papanicholas <jpapanicholas@wayfair.com>
2022-06-17 13:54:19 -0400

    Sniff fixums

--- packages/race-cli/src/reporters/aggregate-console-reporter.ts
+++ packages/race-cli/src/reporters/aggregate-console-reporter.ts
@@ -150,7 +150,7 @@ export class AggregateConsoleReporter extends BaseRacepointReporter {
       const activeStep = this._stepDataCollection[stepKey];
 
       Object.values(LightHouseAuditKeys).forEach((value) => {
-        if (step.lhr.audits.hasOwnProperty(value)) {
+        if (step.lhr.audits[value]) {
           activeStep[value].push(step.lhr.audits[value].numericValue);
         }
       });
