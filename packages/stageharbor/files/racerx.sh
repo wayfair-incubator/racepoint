daa0bed678c5b1427cca0b47e58341f4db86222f
Sergio Armando <cheko@acidbeat.net>
2022-06-28 14:16:48 -0400

    RacerX script :)

--- packages/stageharbor/files/racerx.sh
+++ packages/stageharbor/files/racerx.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+#Racer X Usage, guide to use this script
 showHelp() {
-    echo "Here we should print how to use stage harbor :)"
+    echo
+    echo "Usage: $0 [option...] [parameters]" >&2
+    echo
+    echo "  OPTIONS (At least one must be added):    "
+    echo "  -s,    Measure Startup"
+    echo
+    echo "      Required parameters:    "
+    echo "      -p,    Android Package Name (format com.company.packagename)"
+    echo "      -a,    Android Activity Name (ex MainActivity)"
+    echo
+    echo "      Optional parameters"
+    echo "      -t,    How many times will the app run (ex. 5)"
+    echo "      -c,    Cold boot (Will close the app each run, Warm is default, can"\'"t be used with -h)"
+    echo "      -h,    Hot boot (Will close the app each run, Warm is default, can"\'"t be used with -c)"
+    echo
+    echo " GLOBAL PARAMETERS:   "
+    echo "      -o,    Output to a file (found on /sth/results/output.txt)"
+    echo "      -g,    Show this guide"
+    exit 1
 }
 
 #Regex Helpers
@@ -9,73 +28,133 @@ regexNumbers='^[0-9]+$'
 regexPackageName='^[a-zA-Z]+\.[a-zA-Z]+\.[a-zA-Z]+$'
 regexActivityName='^[a-zA-Z]+$'
 
-#Variables to run the desired processes
+#Control Variables
 hasFlags=0
+optionSelected=0
 startup=0
+output=0
 repeat=1
-packagename=
-activityname=
-apkroute="/mnt/files/debug.apk"
+coldBoot=0
+hotBoot=0
+packageName=
+activityName=
+apkRoute="/mnt/files/debug.apk"
 
-if ! [[ -f $apkroute ]];
+#Validate if we have an apk loaded, so we can run the tests
+if ! [[ -f $apkRoute ]];
 then
     echo "Apk not found, can"\'"t race without a car, aborting."
     exit 1
 fi
 
-while getopts "s p: a: t:" flag;
+#Retrieve the parameters and options provided by the user
+while getopts "s o h g c p: a: t:" flag;
 do
     case $flag in
-    s)  startup=1;;
+    s)  startup=1
+        optionSelected=1;;
     p)  if ! [[ $OPTARG =~ $regexPackageName ]];
         then
             echo "-p Must be followed by a valid package name in this format com.company.packagename"
             exit 1
         fi 
-        packagename=$OPTARG;;
+        packageName=$OPTARG;;
     a)  if [[ -z $OPTARG ]];
         then
             echo "-a Must be followed by an activity name"
             exit 1
         fi
-        activityname=$OPTARG;;
+        activityName=$OPTARG;;
     t)  if ! [[ $OPTARG =~ $regexNumbers ]];
         then
             echo "-t Must be followed by a valid numeric value"
             exit 1
         fi 
         repeat=$OPTARG;;
+    o)  output=1;;
+    h)  hotBoot=1;;
+    g)  showHelp;;
+    c)  coldBoot=1;;
     ?)  echo "Invalid flag found, race is over"
         exit 1;;
     esac
     hasFlags=1
 done
 
-if [[ hasFlags -eq 0 ]];
+#We verify that we have at least one option selected, otherwise we show the usage on the screen
+if [[ $optionSelected -eq 0 ]];
 then
     showHelp
-    exit 1
 fi
 
-#Check complementary variables for required process before running the emulator
-if [[ startup -eq 1 ]] && ( [[ -z $packagename ]] || [[ -z $activityname ]] );
+#Startup option validation for required parameters
+if [[ $startup -eq 1 ]] && ( [[ -z $packageName ]] || [[ -z $activityName ]] );
 then
     echo "Startup option requires -p and -a parameters to run properly"
     exit 1
 fi
 
-echo "Warming up engine (Launching emulator, please wait)..."
+#Startup option validation for optional parameters
+if [[ $startup -eq 1 ]] && ( [[ $coldBoot -eq 1 ]] && [[ $hotBoot -eq 1 ]] );
+then
+    echo "You must choose only one type of boot, either cold or hot or empty to default to warm)"
+    exit 1
+fi
 
-#screen -dm emulator -avd testAVD -no-audio -no-boot-anim -no-window -accel on -gpu off
-#adb wait-for-device
-#adb install -r $apkroute
+#Setup adb server, emulator and installing apk prior to run any command. (We wait until the device is online).
+echo "Warming up engine (Launching emulator, please wait)"
+
+adb kill-server &>/dev/null &
+adb start-server &>/dev/null &
+screen -dm emulator -avd testAVD -no-audio -no-boot-anim -no-window -accel on -gpu off
+adb wait-for-device
+adb install -r $apkRoute
 
 echo "Shooting star running!"
 
+#If startup is the selected option, we run the startup tasks
 if [[ $startup -eq 1 ]];
 then 
-echo "Startup sequence activated..."
+    echo "Startup sequence activated"   
 
+    #The command is built based on the provided options from the user
+    startupSequence="adb shell am start -W -a android.intent.action.VIEW -n $packageName/$packageName.$activityName"
+    
+    #This is the first run, will be discarded to let racepoint save data on cache
+    discardedStartup=$startupSequence
+    discardedStartup+=" -S "
+    discardedStartup+=" &>/dev/null &"
+    eval $discardedStartup
+
+    if [[ $hotBoot -ne 1 ]];
+    then
+        if [[ $repeat -gt 1 ]];
+        then
+            startupSequence+=" -R $repeat"
+        fi
+
+        if [[ $coldBoot -eq 1 ]];
+        then
+            startupSequence+=" -S "
+        fi
+    fi
+    
+    if [[ $output -eq 1 ]];
+    then
+        startupSequence+=" |& tee -a /sth/results/output.txt"
+    fi
+
+    #Cold and Warm runs can be triggered by the am command, for the hot start, we need to navigate to the homescreen (automated in the for loop)
+    if [[ $hotBoot -ne 1 ]];
+    then
+        eval $startupSequence
+    else
+        for i in $(seq 1 $repeat);
+        do
+            adb shell input keyevent KEYCODE_HOME
+            eval $startupSequence
+        done
+    fi
 fi
 
 exit 0
