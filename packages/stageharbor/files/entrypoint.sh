f3deaa5138bc995feb0efdb46f9124b7901977a8
Sergio Armando <cheko@acidbeat.net>
2022-07-02 20:35:09 -0400

    added address resolution by the container from the emulator

--- packages/stageharbor/files/entrypoint.sh
+++ packages/stageharbor/files/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Setting up proxy..."
 bash update-dns.sh
-echo "Applying proxy settings..."
+echo "Applying proxy settings to the container..."
 dnsmasq
 echo "Preparing racerx"
-bash racerx.sh $@
+containerIP=$(awk 'END{print $1}' /etc/hosts)
+racerxScript="bash racerx.sh $@ -d $containerIP"
+eval $racerxScript
--- packages/stageharbor/files/racerx.sh
+++ packages/stageharbor/files/racerx.sh
@@ -20,13 +20,14 @@ showHelp() {
     echo " GLOBAL PARAMETERS:   "
     echo "      -o,    Output to a file (found on /sth/results/output.txt)"
     echo "      -g,    Show this guide"
+    echo "      -d,    DNS Server ip"
     exit 1
 }
 
 #Regex Helpers
 regexNumbers='^[0-9]+$'
-regexPackageName='^[a-zA-Z]+\.[a-zA-Z]+\.[a-zA-Z]+$'
-regexActivityName='^[a-zA-Z]+$'
+regexPackageName='^[a-zA-Z]+\.[a-zA-Z]+\.[a-zA-Z]+.*$'
+regexIP='^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
 
 #Control Variables
 hasFlags=0
@@ -39,6 +40,7 @@ hotBoot=0
 packageName=
 activityName=
 apkRoute="/mnt/files/debug.apk"
+dnsip=
 
 #Validate if we have an apk loaded, so we can run the tests
 if ! [[ -f $apkRoute ]];
@@ -48,7 +50,7 @@ then
 fi
 
 #Retrieve the parameters and options provided by the user
-while getopts "s o h g c p: a: t:" flag;
+while getopts "s o h g c p: a: t: d:" flag;
 do
     case $flag in
     s)  startup=1
@@ -59,9 +61,9 @@ do
             exit 1
         fi 
         packageName=$OPTARG;;
-    a)  if [[ -z $OPTARG ]];
+    a)  if ! [[ $OPTARG =~ $regexPackageName ]];
         then
-            echo "-a Must be followed by an activity name"
+            echo "-a Must be followed by an activity name in this format com.company.packagename.activityname"
             exit 1
         fi
         activityName=$OPTARG;;
@@ -75,6 +77,12 @@ do
     h)  hotBoot=1;;
     g)  showHelp;;
     c)  coldBoot=1;;
+    d)  if ! [[ $OPTARG =~ $regexIP ]];
+        then
+            echo "-d Must be followed by a valid IP Address x.x.x.x"
+            exit 1
+        fi
+        dnsip=$OPTARG;;
     ?)  echo "Invalid flag found, race is over"
         exit 1;;
     esac
@@ -105,9 +113,17 @@ fi
 echo "Warming up engine (Launching emulator, please wait)"
 
 adb kill-server &>/dev/null &
-adb start-server &>/dev/null &
 screen -dm emulator -avd testAVD -no-audio -no-boot-anim -no-window -accel on -gpu off
+sleep 5
 adb wait-for-device
+
+if [[ -n $dnsip ]];
+then
+    proxyconfig="adb shell settings put global http_proxy $dnsip"
+    echo "Routing all DNS calls to $dnsip"
+    eval $proxyconfig
+fi
+
 adb install -r $apkRoute
 
 echo "Shooting star running!"
@@ -118,7 +134,7 @@ then
     echo "Startup sequence activated"   
 
     #The command is built based on the provided options from the user
-    startupSequence="adb shell am start -W -a android.intent.action.VIEW -n $packageName/$packageName.$activityName"
+    startupSequence="adb shell am start -W -a android.intent.action.VIEW -n $packageName/$activityName"
     
     #This is the first run, will be discarded to let racepoint save data on cache
     discardedStartup=$startupSequence
@@ -128,19 +144,23 @@ then
 
     if [[ $hotBoot -ne 1 ]];
     then
-        if [[ $repeat -gt 1 ]];
-        then
-            startupSequence+=" -R $repeat"
-        fi
 
         if [[ $coldBoot -eq 1 ]];
         then
             startupSequence+=" -S "
         fi
+
+        if [[ $repeat -gt 1 ]];
+        then
+            startupSequence+=" -R $repeat"
+        fi
+
+       
     fi
     
     if [[ $output -eq 1 ]];
     then
+        > /sth/results/output.txt
         startupSequence+=" |& tee -a /sth/results/output.txt"
     fi
 
@@ -151,6 +171,7 @@ then
     else
         for i in $(seq 1 $repeat);
         do
+            sleep 2
             adb shell input keyevent KEYCODE_HOME
             eval $startupSequence
         done
